a4effac143ffd3fd1a77d9d6331b133485112ea2
paulo535180 <paulo535180@gmail.com>
2022-07-08 16:09:16 -0300

    Aula 92

--- JavaScript - Funcao/01. cidadaoPrimeiraClasse.js
+++ JavaScript - Funcao/01. cidadaoPrimeiraClasse.js
@@ -2,10 +2,34 @@
 //Higher-order function
 
 //Criar de forma literal
-function func1(){}
+function func1(){return 'Função 1'}
 
 //Armazendar em uma variavel
-const func2 = function(){}
+const func2 = function(){return 'Função 2'}
 
 //Armazenar dentro de um array
 const array = [function (a,b) {return a + b}, func1, func2]
+console.log(array[0](2,3), array[1](),array[2]())
+
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function (){return 'Opa!'}
+console.log(obj.falar())
+
+//Passar função como parametro
+function run (fun){
+    fun();
+}
+run(function(){
+    console.log('Executando...')
+})
+
+//Uma função pode retornar/conter uma função
+function soma(a,b){
+    return function(c){
+        console.log(a+b+c)
+    }
+}
+
+soma(5,5)(1)
